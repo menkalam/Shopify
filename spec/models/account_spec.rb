6f84dc27d0a38a549eaff7982e4d1681fd9fb051
Sean Frederick <sean.frederick.1@gmail.com>
2014-07-04 23:17:24 -0500

    Account model and related files

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Account, :type => :model do
+let(:valid_attributes) { { "shopify_account_url" => "MyString", "shopify_password" => "MyString", "shopify_api_key" => "MyString", "shopify_shared_secret" => "MyString" } }
+end
